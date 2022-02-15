51cef11d5e8842509c5152b260de186cda4f13b4
HugoDelgado18 <39805296+HugoDelgado18@users.noreply.github.com>
2022-02-14 21:23:57 -0600

    basic get route to / set and working

---
+++ server/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express();
+
+
+app.use(express.urlencoded({extended: true}));
+
+
+app.route('/')
+  .get(function(req, res) {
+    res.send('Success')
+  });
+
+
+  const port = 3000;
+
+  app.listen(port, function(){
+    console.log('Successfully opened port:3000');
+  })
