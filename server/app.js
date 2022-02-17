e69c4f9c4445298d04ca641c29d07def5ea2dd1b
HugoDelgado18 <39805296+HugoDelgado18@users.noreply.github.com>
2022-02-17 13:29:31 -0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -1,4 +1,6 @@
+
 import express from 'express';
+import https from 'https';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
@@ -14,9 +16,50 @@ async function main(){
   mongoose.connect('mongodb://localhost:27017/cbCafeDB')
 }
 
+const reviewSchema = new mongoose.schema({
+  name: String,
+  rating: {
+    type: Number,
+    required: {$gte: 5}
+  },
+  content: String
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+
+const review = new Review({
+  name: Leah,
+  rating: 5,
+  content: "This place is great!!"
+})
+
+
+
+
 app.route('/')
   .get(function(req, res) {
+
     res.send('Success')
+  })
+  .post(function(req, res){
+    let rName = req.body.reviewName;
+    let rRating = req.body.reviewRating;
+    let rContent = req.body.reviewContent;
+
+    const reviewNew = new Review({
+      name: rName,
+      rating: rRating,
+      content: rContent
+    })
+
+    reviewNew.save(function(err){
+      if (err){
+        console.log(err);
+      } else {
+        res.redirect("/")
+      }
+    })
+
   });
 
 
