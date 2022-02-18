b74f3f2599ce5e475c84fe5f7dd62561a18da87e
Hugo Delgado <hugo.delgado1@outlook.com>
2022-02-17 20:34:46 -0600

    last commit incomplete

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom';
 
+import App from './App';
 
-ReactDOM.render(
-  <div>
-    <h1>  </h1>
-  </dv>,
-  document.getElementById('root')
-);
-export
+ReactDOM.render(<App />, document.getElementById('root'));
