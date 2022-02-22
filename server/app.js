fbfb83a3eb1b9f85bf563a68590631249d25c712
Hugo Delgado <hugo.delgado1@outlook.com>
2022-02-22 10:18:30 -0600

    atlas up and running

--- server/app.js
+++ server/app.js
@@ -1,4 +1,4 @@
-
+import 'dotenv/config';
 import express from 'express';
 import https from 'https';
 import cors from 'cors';
@@ -8,63 +8,64 @@ import mongoose from 'mongoose';
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
+app.use(cors());
 // app.use("/public", express.static(__dirname + "/public"));
 
 main().catch(err => console.log(err));
 
 async function main(){
-  mongoose.connect('mongodb://localhost:27017/cbCafeDB')
-}
+  mongoose.connect(process.env.ATLAS_KEY)
+};
 
-const reviewSchema = new mongoose.schema({
-  name: String,
-  rating: {
-    type: Number,
-    required: {$gte: 5}
-  },
-  content: String
-});
+// const reviewSchema = new mongoose.schema({
+//   name: String,
+//   rating: {
+//     type: Number,
+//     required: {$gte: 5}
+//   },
+//   content: String
+// });
+//
+// const Review = mongoose.model("Review", reviewSchema);
+//
+// const review = new Review({
+//   name: Leah,
+//   rating: 5,
+//   content: "This place is great!!"
+// })
+//
+//
+//
+//
+// app.route('/')
+//   .get(function(req, res) {
+//
+//     res.send('Success')
+//   })
+//   .post(function(req, res){
+//     let rName = req.body.reviewName;
+//     let rRating = req.body.reviewRating;
+//     let rContent = req.body.reviewContent;
+//
+//     const reviewNew = new Review({
+//       name: rName,
+//       rating: rRating,
+//       content: rContent
+//     })
+//
+//     reviewNew.save(function(err){
+//       if (err){
+//         console.log(err);
+//       } else {
+//         res.redirect("/")
+//       }
+//     })
+//
+//   });
 
-const Review = mongoose.model("Review", reviewSchema);
-
-const review = new Review({
-  name: Leah,
-  rating: 5,
-  content: "This place is great!!"
-})
 
 
 
-
-app.route('/')
-  .get(function(req, res) {
-
-    res.send('Success')
-  })
-  .post(function(req, res){
-    let rName = req.body.reviewName;
-    let rRating = req.body.reviewRating;
-    let rContent = req.body.reviewContent;
-
-    const reviewNew = new Review({
-      name: rName,
-      rating: rRating,
-      content: rContent
-    })
-
-    reviewNew.save(function(err){
-      if (err){
-        console.log(err);
-      } else {
-        res.redirect("/")
-      }
-    })
-
-  });
-
-
-  const port = 3000;
-
-  app.listen(port, function(){
+  app.listen(process.env.PORT || 3000, function(){
     console.log('Successfully opened port:3000');
   })
